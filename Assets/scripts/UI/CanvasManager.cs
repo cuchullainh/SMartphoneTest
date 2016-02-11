a4edc9766621335ea4c1c184aeb2b728498e4bea
cuchullainh <matti-krebs@web.de>
2016-02-11 13:54:36 +0100

    Startmenu und Ui

---
+++ Assets/scripts/UI/CanvasManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanvasManager : MonoBehaviour
+{
+
+    public void Exit()
+    {
+        print("BUH");
+        Application.Quit();
+    }
+}
